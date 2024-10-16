96c0a0ccdf8cb79fe9b4ccd1443ee41a06abe573
Varpenikhil0007 <varpenikhil0007@gmail.com>
2024-10-16 18:32:57 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ESLint } from 'eslint'
 
-const viteConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+
 
 
 // https://vitejs.dev/config/
